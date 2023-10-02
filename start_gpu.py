9b6e5932c17a78b1337be829a891c081213e6a3a
Aqua <58160105+0xAqua0@users.noreply.github.com>
2023-10-03 04:27:36 +0800

    add scheduled mining fuction

--- start_gpu.py
+++ start_gpu.py
@@ -1,4 +1,4 @@
-import time, json, requests, time, hashlib, string, threading, re, configparser, os, psutil
+import time, json, requests, time, hashlib, string, threading, re, configparser, os, re, psutil
 from passlib.hash import argon2
 from random import choice, randrange
 from datetime import datetime
@@ -56,6 +56,16 @@ gpu_count = pynvml.nvmlDeviceGetCount()
 gpu_handles = []
 for i in range(gpu_count):
     gpu_handles.append(pynvml.nvmlDeviceGetHandleByIndex(i))
+
+# Mining Tasks
+task_list = []
+
+def add_task(account_address, blocks_to_mine):
+    if re.match("^0x[0-9a-fA-F]{40}$", account_address) and float(blocks_to_mine) > 0:
+        task_list.append([account_address, int(blocks_to_mine)])
+        print(f"Task added: Mining {blocks_to_mine} for {account_address}")
+    else:
+        raise KeyError(f"{account_address} is not a vaild ethereum address")
 
 
 def hash_value(value):
@@ -121,36 +131,6 @@ def get_pid_by_name(pname):
     return res
 
 
-def monitor_miner_process():
-    # Check the miner process periodically   
-    while True:     
-        miner_list = get_pid_by_name('xen')
-        if len(miner_list) > 0:
-            # check all gpu mem
-            memtotal = 0
-            memused = 0
-            for gpu in gpu_handles:
-                meminfo = pynvml.nvmlDeviceGetMemoryInfo(gpu)
-                memtotal += int(meminfo.total/1024**3)
-                memused += round(meminfo.used/1024**3, 3)
-            memused_pct = memused / memtotal
-            if memused_pct < 0.60 or memused_pct > 0.996:
-                print(f"\nMemory not in efficiency range ({memused} / {memtotal}). Restarting...")
-                for pid in miner_list:
-                    os.system(f"kill {pid}")
-            time.sleep(1)
-            miner_list = get_pid_by_name('xen')
-
-        if len(miner_list) == 0:
-            # batch_size = int(memtotal * 0.825 * 1024 ** 2 / int(updated_memory_cost))
-            print(f"\nNo miners are running. Starting...")
-            for i in range(gpu_count):
-                os.system(f"nohup ./xen -d{i} > miner_stat_{i}.out &")
-                print(f"Miner is running on GPU {i}")
-
-        time.sleep(10)
-
-
 def submit_pow(account_address, key, hash_to_verify):
     # Download last block record
     try:
@@ -158,7 +138,7 @@ def submit_pow(account_address, key, hash_to_verify):
         response = requests.get(f"{server_url}:4445/getblocks/lastblock", timeout=10)  # Adding a timeout of 10 seconds
     except requests.exceptions.RequestException as e:
         # Handle any exceptions that occur during the request
-        print(f"An error occurred: {e}")
+        print(f"An error occurred while fetching blocks: {e}")
         return None  # Optionally return an error value or re-raise the exception
 
     if response.status_code != 200:
@@ -223,17 +203,16 @@ RESET = "\033[0m"
 normal_blocks_count = 0
 super_blocks_count = 0
 xuni_blocks_count = 0
-def submit_block(key):
+def submit_block(account_address, key):
     global updated_memory_cost  # Make it global so that we can update it
-    found_valid_hash = False
-
     global normal_blocks_count
     global super_blocks_count
     global xuni_blocks_count
 
     argon2_hasher = argon2.using(time_cost=difficulty, salt=b"XEN10082022XEN", memory_cost=updated_memory_cost, parallelism=cores, hash_len = 64)
     hashed_data = argon2_hasher.hash(key)
-
+    found_valid_hash = False
+    
     for target in stored_targets:
         if target in hashed_data[-87:]:
         # Search for the pattern "XUNI" followed by a digit (0-9)
@@ -256,7 +235,7 @@ def submit_block(key):
         payload = {
             "hash_to_verify": hashed_data,
             "key": key,
-            "account": account,
+            "account": account_address,
             "attempts": "130000",
             "hashes_per_second": "1000",
             "worker": worker_id  # Adding worker information to the payload
@@ -272,19 +251,16 @@ def submit_block(key):
             print("HTTP Status Code:", response.status_code)
 
             if found_valid_hash and response.status_code == 200:
-                if "XUNI" in hashed_data:
+                if target == "XUNI":
                     xuni_blocks_count += 1
-                    break
-                elif "XEN11" in hashed_data:
+                elif target == "XEN11":
                     capital_count = sum(1 for char in re.sub('[0-9]', '', hashed_data) if char.isupper())
                     if capital_count >= 65:
                         super_blocks_count += 1
                     else:
                         normal_blocks_count += 1
-
-            if target == "XEN11" and found_valid_hash and response.status_code == 200:
-                #submit proof of work validation of last sealed block
-                submit_pow(account, key, hashed_data)
+                    #submit proof of work validation of last sealed block
+                    submit_pow(account_address, key, hashed_data)
 
             if response.status_code != 500:  # If status code is not 500, break the loop
                 print("Server Response:", response.json())
@@ -297,36 +273,100 @@ def submit_block(key):
             try:
                 print("Server Response:", response.json())
             except Exception as e:
-                print("An error occurred:", e)
+                print("An error occurred while parsing server response:", e)
 
-    return key, hashed_data
+    return key, target, hashed_data
 
 
 def monitor_blocks_directory():
-    counter = 0
+    global task_list
     with tqdm(total=None, dynamic_ncols=True, desc=f"{GREEN}Mining{RESET}", unit=f" {GREEN}Blocks{RESET}") as pbar:
         while True:
             XENDIR = f"gpu_found_blocks_tmp/"
+            mining_account = account
+
+            if len(task_list):
+                while task_list[0][1] <= 0:
+                    del task_list[0]
+                mining_account = task_list[0][0]
+
             if not os.path.exists(XENDIR):
                 os.makedirs(XENDIR)
             for filename in os.listdir(XENDIR):
                 filepath = os.path.join(XENDIR, filename)
                 with open(filepath, 'r') as f:
                     data = f.read()
-                submit_block(data)
+                # submit block using current mining account
+                k, target, hashed = submit_block(mining_account, data)
                 os.remove(filepath)
+                if target == 'XEN11':
+                    task_list[0][1] -= 1
                 pbar.update(1)
 
             superblock = f"{RED}super:{super_blocks_count}{RESET} "
             block = f"{GREEN}normal:{normal_blocks_count}{RESET} "
-            xuni = f"{BLUE}xuni:{xuni_blocks_count}{RESET} "
+            xuni = f"{BLUE}xuni:{xuni_blocks_count}{RESET}"
+            current_account = f"Mining on: {mining_account} "
+            remaining = f"Remaining: {task_list[0][1] if len(task_list) else 'inf'}"
             
             if super_blocks_count == 0 and normal_blocks_count == 0 and xuni_blocks_count == 0:
-                pbar.set_postfix({"Details": f"No blocks were mined."}, refresh=True)
+                pbar.set_postfix({"Details": f"No blocks were mined. {current_account}{remaining}"}, refresh=True)
             else:
-                pbar.set_postfix({"Details": f"{superblock}{block}{xuni}"}, refresh=True)
+                pbar.set_postfix({"Details": f"{superblock}{block}{xuni} | {current_account}{remaining}"}, refresh=True)
 
             time.sleep(0.8)  # Check every 1 seconds
+
+
+def monitor_task_files():
+    while True:
+        TASKFILE = f"task.in"
+        if not os.path.exists(TASKFILE):
+            os.system(f"touch {TASKFILE}")
+        else:
+            with open(TASKFILE, 'r') as f:
+                content = f.read()
+            if content:
+                tasks = content.split('\n')
+                for task in tasks:
+                    props = task.split(' ')
+                    if len(props) == 2: 
+                        try:
+                            add_task(props[0], props[1]) 
+                        except Exception as e:
+                            print(f"An error occured while adding tasks: {e}")
+                os.system(f"cat /dev/null > {TASKFILE}")
+            
+        time.sleep(10)
+
+
+def monitor_miner_process():
+    # Check the miner process periodically   
+    while True:     
+        miner_list = get_pid_by_name('xen')
+        if len(miner_list) > 0:
+            # check all gpu mem
+            memtotal = 0
+            memused = 0
+            for gpu in gpu_handles:
+                meminfo = pynvml.nvmlDeviceGetMemoryInfo(gpu)
+                memtotal += int(meminfo.total/1024**3)
+                memused += round(meminfo.used/1024**3, 3)
+            memused_pct = memused / memtotal
+            if memused_pct < 0.60 or memused_pct > 0.996:
+                print(f"\nMemory used is not in efficiency range ({memused} / {memtotal}). Restarting...")
+                for pid in miner_list:
+                    os.system(f"kill {pid}")
+            time.sleep(1)
+            miner_list = get_pid_by_name('xen')
+
+        if len(miner_list) == 0:
+            # batch_size = int(memtotal * 0.95 * 1024 ** 2 / int(updated_memory_cost))
+            print(f"\nNo miners are running. Starting...")
+            for i in range(gpu_count):
+                os.system(f"nohup ./xen -d{i} > miner_stat_{i}.out &")
+                print(f"Miner is running on GPU {i}")
+
+        time.sleep(10)
 
 
 if __name__ == "__main__":
@@ -341,6 +381,10 @@ if __name__ == "__main__":
     miner_thread = threading.Thread(target=monitor_miner_process)
     miner_thread.daemon = True
     miner_thread.start()
+
+    task_thread = threading.Thread(target=monitor_task_files)
+    task_thread.daemon = True
+    task_thread.start()
 
     submit_thread = threading.Thread(target=monitor_blocks_directory)
     submit_thread.daemon = True  # This makes the thread exit when the main program exits
