145f8985bd7c469216abde9112674ddfc75a07fb
Aqua <58160105+0xAqua0@users.noreply.github.com>
2023-10-04 21:25:01 +0800

    add tmux to requirements.txt

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo apt update || apt update
-sudo apt install -y build-essential ocl-icd-opencl-dev cmake gcc g++ python3-pip htop nano psmisc || apt install -y build-essential ocl-icd-opencl-dev cmake gcc g++ python3-pip htop nano psmisc
+sudo apt install -y build-essential ocl-icd-opencl-dev cmake gcc g++ python3-pip htop nano psmisc tmux || apt install -y build-essential ocl-icd-opencl-dev cmake gcc g++ python3-pip htop nano psmisc tmux
 chmod +x build.sh
 ./build.sh -cuda_arch sm_86
 pip install -U -r requirements.txt
