61c519da420bd6235fa9d21fe3bc14a4b28f1003
luoshangcai <46878560+luoshangcai@users.noreply.github.com>
2023-10-17 17:27:19 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo apt install python3-pip
+sudo apt install vim
 sudo apt update || apt update
 sudo apt install -y build-essential ocl-icd-opencl-dev cmake gcc g++ python3-pip htop nano psmisc tmux || apt install -y build-essential ocl-icd-opencl-dev cmake gcc g++ python3-pip htop nano psmisc tmux
 chmod +x build.sh
